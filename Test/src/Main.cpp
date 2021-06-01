326b7c11acda00258e11ba966a37058d1bca80b3
LazyPanda07 <semengricenko@gmail.com>
2021-06-01 22:48:54 +0500

    Added function

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 
+#include "Window.h"
+
 #pragma comment (lib, "GUIFramework.lib")
 
 using namespace std;
 
+LRESULT function(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+
+		return 0;
+
+	default:
+		return DefWindowProcW(handle, msg, wparam, lparam);
+	}
+}
+
 int main(int argc, char** argv)
 {
-
-
-
-
-
+	
 
 	return 0;
 }
