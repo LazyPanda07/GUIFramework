cfdd0f6c363e20fd305f4d10d86cc09fd62619cf
LazyPanda07 <semengricenko@gmail.com>
2021-06-02 04:12:34 +0500

    Added creation window with sub window

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-#include "SeparateWindow.h"
+#include "Composites/BaseSeparateWindow.h"
+#include "Composites/BaseChildWindow.h"
 
 #pragma comment (lib, "GUIFramework.lib")
 
@@ -9,7 +10,13 @@ using namespace gui_framework;
 
 void test(const wstring& className, const wstring& title, const string& functionName, int x, int y)
 {
-	SeparateWindow window(className, title, functionName, x, y, 800, 600);
+	utility::ComponentSettings settings(WS_BORDER, x, y, 800, 600);
+	utility::ComponentSettings insideWindowSettings(WS_BORDER, 100, 100, 400, 400);
+	
+	BaseSeparateWindow window(className, title, settings, functionName);
+	BaseChildWindow* inside = new BaseChildWindow(L"Inside", L"Inside", insideWindowSettings, &window, "insideWindow");
+	
+	window.addChild(inside);
 
 	MSG msg = {};
 
@@ -21,11 +28,13 @@ void test(const wstring& className, const wstring& title, const string& function
 	}
 }
 
-CREATE_DEFAULT_SEPARATE_WINDOW_FUNCTION(mainWindow)
+CREATE_DEFAULT_WINDOW_FUNCTION(mainWindow)
+
+CREATE_DEFAULT_WINDOW_FUNCTION(insideWindow)
 
 int main(int argc, char** argv)
 {
-	thread(test, L"Main window", L"Title", "mainWindow", 600, 450).detach();
+	thread(test, L"Main window", L"Title", "mainWindow", 300, 200).detach();
 
 	string s;
 
