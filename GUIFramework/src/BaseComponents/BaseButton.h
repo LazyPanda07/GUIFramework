a35696c4e89bc9a31f7a7ee97646552d6736fd11
LazyPanda07 <semengricenko@gmail.com>
2021-06-02 04:48:26 +0500

    Fixed

--- GUIFramework/src/BaseComponents/BaseButton.h
+++ GUIFramework/src/BaseComponents/BaseButton.h
@@ -13,7 +13,7 @@ namespace gui_framework
 	public:
 		BaseButton(const std::wstring& buttonName, const utility::ComponentSettings& settings, BaseComponent* parent, uint32_t buttonId, const std::function<LRESULT(WPARAM, LPARAM)>& onClick = nullptr);
 
-		virtual LRESULT windowMessagesHandle(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam) = 0;
+		virtual LRESULT windowMessagesHandle(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam, bool& isUsed) = 0;
 
 		virtual void setOnClick(const std::function<LRESULT(WPARAM, LPARAM)>& onClick) final;
 
