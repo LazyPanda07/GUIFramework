520a7de005073408c6edf9b365729f3bfd0d714a
LazyPanda07 <semengricenko@gmail.com>
2021-06-01 23:48:54 +0500

    Refactor

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -1,28 +1,48 @@
 #include <iostream>
 
-#include "Window.h"
+#include "SeparateWindow.h"
 
 #pragma comment (lib, "GUIFramework.lib")
 
 using namespace std;
+using namespace gui_framework;
 
-LRESULT function(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam)
+void test(const pair<wstring, wstring>& data, int x, int y)
 {
-	switch (msg)
+	SeparateWindow window(data.first, data.second, x, y, 800, 600);
+
+	MSG msg = {};
+
+	while (GetMessageW(&msg, nullptr, NULL, NULL))
 	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
+		TranslateMessage(&msg);
 
-		return 0;
-
-	default:
-		return DefWindowProcW(handle, msg, wparam, lparam);
+		DispatchMessageW(&msg);
 	}
 }
 
 int main(int argc, char** argv)
 {
-	
+	string s;
+	vector<pair<wstring, wstring>> windows =
+	{
+		{ L"First", L"First" },
+		{ L"Second", L"Second" },
+		{ L"Third", L"Third" },
+		{ L"Fourth", L"Fourth" }
+	};
+	size_t i = 0;
+
+	while (cin >> s)
+	{
+		if (i < windows.size())
+		{
+			thread(test, ref(windows[i++]), rand() % 1600, rand() % 900).detach();
+		}
+	}
+
+
+
 
 	return 0;
 }
