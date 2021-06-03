2b745aa12cc3e1e4662c8a7a2691a9575d28050b
LazyPanda07 <semengricenko@gmail.com>
2021-06-03 20:41:50 +0500

    Added ITextOperations interface

---
+++ GUIFramework/src/Interfaces/Components/ITextOperations.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "pch.h"
+
+namespace gui_framework
+{
+	namespace interfaces
+	{
+		class ITextOperations
+		{
+		protected:
+			HWND textHandle;
+
+		public:
+			ITextOperations(HWND handle);
+
+			virtual void setText(const std::wstring& text) const final;
+
+			/// @brief Get text from control
+			/// @return Text from control
+			/// @exception gui_framework::exceptions::GetLastErrorException
+			virtual std::wstring getText() const final;
+
+			virtual ~ITextOperations() = default;
+		};
+	}
+}
