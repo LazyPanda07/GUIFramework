a35510859b003f3b05b4fd2d58aad0040304d9cd
LazyPanda07 <semengricenko@gmail.com>
2021-06-04 21:49:47 +0500

    Refactor

--- GUIFramework/src/Components/StaticControl.cpp
+++ GUIFramework/src/Components/StaticControl.cpp
@@ -6,6 +6,20 @@ using namespace std;
 namespace gui_framework
 {
 	StaticControl::StaticControl(const wstring& staticControlName, const wstring& editControlText, int x, int y, BaseComponent* parent, int width, int height) :
+		BaseComponent
+		(
+			wstring(standard_classes::button),
+			staticControlName,
+			utility::ComponentSettings
+			(
+				NULL,
+				x,
+				y,
+				width,
+				height
+			),
+			parent
+		),
 		BaseResizableComponent
 		(
 			wstring(standard_classes::staticControl),
