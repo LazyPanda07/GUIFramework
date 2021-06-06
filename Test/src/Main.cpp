e6761ed8063ad4c823c68b98bf26aba1d52c0d8f
LazyPanda07 <semengricenko@gmail.com>
2021-06-06 16:04:47 +0500

    Refactor

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -2,7 +2,7 @@
 
 #include "Composites/SeparateWindow.h"
 #include "Composites/ChildWindow.h"
-#include "Components/ComboBox.h"
+#include "Components/SimpleComboBox.h"
 
 #pragma comment (lib, "GUIFramework.lib")
 
@@ -13,10 +13,13 @@ void test(const wstring& className, const wstring& title, const string& function
 	using namespace gui_framework;
 
 	utility::ComponentSettings settings(WS_BORDER, x, y, 800, 600);
-	utility::ComponentSettings childWindowSettings(WS_BORDER | CBS_SIMPLE, 25, 25, 40, 120);
 
 	unique_ptr<SeparateWindow> mainWindow(make_unique<SeparateWindow>(className, title, settings, functionName));
-	BaseComboBox* comboBox = new BaseComboBox(L"Combo", childWindowSettings, mainWindow.get());
+	SimpleComboBox* comboBox = new SimpleComboBox(L"Combo", 25, 25, 40, 120, mainWindow.get());
+
+	comboBox->addValue(L"First");
+	comboBox->addValue(L"Second");
+	comboBox->addValue(L"Third");
 
 	mainWindow->addChild(comboBox);
 
