b7089494844a4c186f5363841e1f90f17956839d
LazyPanda07 <semengricenko@gmail.com>
2021-06-09 03:59:35 +0500

    Added vertical and horizontal scrolls

--- GUIFramework/src/Components/RichEdit.h
+++ GUIFramework/src/Components/RichEdit.h
@@ -7,7 +7,7 @@ namespace gui_framework
 	class RichEdit : public BaseRichEdit
 	{
 	public:
-		RichEdit(const std::wstring& richEditName, int x, int y, uint16_t width, uint16_t height, BaseComponent* parent, bool isMultiLine);
+		RichEdit(const std::wstring& richEditName, int x, int y, uint16_t width, uint16_t height, BaseComponent* parent, bool isMultiLine = false);
 
 		~RichEdit() = default;
 	};
