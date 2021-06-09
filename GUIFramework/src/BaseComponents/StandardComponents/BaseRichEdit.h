3e92b38a1249f5625c8fbfef0e8c28d6cfe864b5
LazyPanda07 <semengricenko@gmail.com>
2021-06-09 17:05:06 +0500

    Added url detection

--- GUIFramework/src/BaseComponents/StandardComponents/BaseRichEdit.h
+++ GUIFramework/src/BaseComponents/StandardComponents/BaseRichEdit.h
@@ -12,7 +12,36 @@ namespace gui_framework
 		public interfaces::ITextOperations
 	{
 	public:
+		enum class urlDetectEvent
+		{
+			mouseMove = WM_MOUSEMOVE,
+			leftButtonDown,
+			leftButtonUp,
+			leftButtonDoubleClick,
+			rightButtonDown,
+			rightButtonUp,
+			rightButtonDoubleClick,
+			setCursor = WM_SETCURSOR
+		};
+
+		static constexpr uint8_t urlDetectEventSize = 8;
+
+	protected:
+		std::unordered_map<urlDetectEvent, std::function<void(const std::wstring&)>> callbacks;
+
+	protected:
+		virtual LRESULT preWindowMessagesHandle(HWND handle, UINT message, WPARAM wparam, LPARAM lparam, bool& isUsed) override;
+
+	public:
 		BaseRichEdit(const std::wstring& windowName, const utility::ComponentSettings& settings, BaseComponent* parent);
+
+		virtual void addUrlDetectEvent(urlDetectEvent event, const std::function<void(const std::wstring&)>& eventCallback) final;
+
+		virtual void removeUrlDetectEvent(urlDetectEvent event) final;
+
+		virtual void setAutoURLDetect(bool autoURLDetect) final;
+
+		virtual bool getAutoURLDetect() const final;
 
 		virtual ~BaseRichEdit() = default;
 	};
