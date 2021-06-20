09a616e5521f7de99f2067713aec4c014fd18018
LazyPanda07 <semengricenko@gmail.com>
2021-06-20 05:28:04 +0500

    Fixed, added insert, set, get, clear methods

--- GUIFramework/src/BaseComponents/StandardComponents/BaseTabControl.cpp
+++ GUIFramework/src/BaseComponents/StandardComponents/BaseTabControl.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 namespace gui_framework
 {
-	BaseTabControl::BaseTabControl(const wstring& tabControlName, const utility::ComponentSettings& settings, BaseComponent* parent) :
+	BaseTabControl::BaseTabControl(const wstring& tabControlName, const utility::ComponentSettings& settings, uint16_t iconWidth, uint16_t iconHeight, BaseComponent* parent) :
 		BaseComponent
 		(
 			wstring(standard_classes::tabControl),
@@ -15,67 +15,136 @@ namespace gui_framework
 			settings,
 			parent
 		),
-		imageList(ImageList_Create(standard_sizes::defaultTabControlImageWidth, standard_sizes::defaultTabControlImageHeight, ILC_COLOR32, 0, standard_sizes::defaultImagesCount))
+		imageList(ImageList_Create(iconWidth, iconHeight, ILC_COLOR32, 0, standard_sizes::defaultImagesCount)),
+		iconWidth(iconWidth),
+		iconHeight(iconHeight)
 	{
-		SendMessageW(handle, TCM_SETIMAGELIST, NULL, reinterpret_cast<LPARAM>(handle));
+		SendMessageW(handle, TCM_SETIMAGELIST, NULL, reinterpret_cast<LPARAM>(imageList));
 	}
 
-	LRESULT BaseTabControl::append(const wstring& text)
+	LRESULT BaseTabControl::appendText(const wstring& text)
 	{
-		size_t currentSize = this->size();
+		return this->insertText(this->size(), text);
+	}
+
+	LRESULT BaseTabControl::insertText(size_t index, const wstring& text)
+	{
 		TCITEMW item = {};
 
+		item.mask = TCIF_TEXT;
 		item.pszText = const_cast<wchar_t*>(text.data());
 		item.cchTextMax = static_cast<int>(text.size());
-		item.iImage = -1;
 
-		return SendMessageW(handle, TCM_INSERTITEM, currentSize, reinterpret_cast<LPARAM>(&item));
+		return SendMessageW(handle, TCM_INSERTITEM, index, reinterpret_cast<LPARAM>(&item));
 	}
 
-	LRESULT BaseTabControl::append(const filesystem::path& pathToImage)
+	LRESULT BaseTabControl::appendImage(const filesystem::path& pathToImage)
+	{
+		return this->insertImage(this->size(), pathToImage);
+	}
+
+	LRESULT BaseTabControl::insertImage(size_t index, const filesystem::path& pathToImage)
 	{
 		if (!filesystem::exists(pathToImage))
 		{
 			throw exceptions::FileDoesNotExist(pathToImage);
 		}
 
-		size_t currentSize = this->size();
 		TCITEMW item = {};
 
 		if (!images.contains(pathToImage))
 		{
-			HBITMAP image = static_cast<HBITMAP>(LoadImageW(nullptr, pathToImage.wstring().data(), IMAGE_BITMAP, standard_sizes::defaultTabControlImageWidth, standard_sizes::defaultTabControlImageHeight, LR_LOADFROMFILE));
+			HBITMAP image = static_cast<HBITMAP>(LoadImageW(nullptr, pathToImage.wstring().data(), IMAGE_BITMAP, iconWidth, iconHeight, LR_LOADFROMFILE));
 
 			images[pathToImage] = ImageList_Add(imageList, image, NULL);
 		}
 
+		item.mask = TCIF_IMAGE;
 		item.iImage = images[pathToImage];
 
-		return SendMessageW(handle, TCM_INSERTITEM, currentSize, reinterpret_cast<LPARAM>(&item));
+		return SendMessageW(handle, TCM_INSERTITEM, index, reinterpret_cast<LPARAM>(&item));
 	}
 
-	LRESULT BaseTabControl::append(const wstring& text, const filesystem::path& pathToImage)
+	LRESULT BaseTabControl::appendTextAndImage(const wstring& text, const filesystem::path& pathToImage)
+	{
+		return this->insertTextAndImage(this->size(), text, pathToImage);
+	}
+
+	LRESULT BaseTabControl::insertTextAndImage(size_t index, const wstring& text, const filesystem::path& pathToImage)
 	{
 		if (!filesystem::exists(pathToImage))
 		{
 			throw exceptions::FileDoesNotExist(pathToImage);
 		}
 
-		size_t currentSize = this->size();
 		TCITEMW item = {};
 
 		if (!images.contains(pathToImage))
 		{
-			HBITMAP image = static_cast<HBITMAP>(LoadImageW(nullptr, pathToImage.wstring().data(), IMAGE_BITMAP, standard_sizes::defaultTabControlImageWidth, standard_sizes::defaultTabControlImageHeight, LR_LOADFROMFILE));
+			HBITMAP image = static_cast<HBITMAP>(LoadImageW(nullptr, pathToImage.wstring().data(), IMAGE_BITMAP, iconWidth, iconHeight, LR_LOADFROMFILE));
 
 			images[pathToImage] = ImageList_Add(imageList, image, NULL);
 		}
 
+		item.mask = TCIF_TEXT | TCIF_IMAGE;
 		item.pszText = const_cast<wchar_t*>(text.data());
 		item.cchTextMax = static_cast<int>(text.size());
 		item.iImage = images[pathToImage];
 
-		return SendMessageW(handle, TCM_INSERTITEM, currentSize, reinterpret_cast<LPARAM>(&item));
+		return SendMessageW(handle, TCM_INSERTITEM, index, reinterpret_cast<LPARAM>(&item));
+	}
+
+	bool BaseTabControl::setItem(size_t index, const wstring& text, const filesystem::path& pathToImage)
+	{
+		if (!pathToImage.empty() && !filesystem::exists(pathToImage))
+		{
+			throw exceptions::FileDoesNotExist(pathToImage);
+		}
+
+		TCITEMW item = {};
+
+		if (text.size())
+		{
+			item.mask |= TCIF_TEXT;
+
+			item.pszText = const_cast<wchar_t*>(text.data());
+			item.cchTextMax = static_cast<int>(text.size());
+		}
+
+		if (!pathToImage.empty())
+		{
+			if (!images.contains(pathToImage))
+			{
+				HBITMAP image = static_cast<HBITMAP>(LoadImageW(nullptr, pathToImage.wstring().data(), IMAGE_BITMAP, iconWidth, iconHeight, LR_LOADFROMFILE));
+
+				images[pathToImage] = ImageList_Add(imageList, image, NULL);
+			}
+
+			item.mask |= TCIF_IMAGE;
+
+			item.iImage = images[pathToImage];
+		}
+
+		return SendMessageW(handle, TCM_SETITEM, index, reinterpret_cast<LPARAM>(&item));
+	}
+
+	TCITEMW BaseTabControl::getItem(size_t index)
+	{
+		TCITEMW item = {};
+
+		SendMessageW(handle, TCM_GETITEM, static_cast<WPARAM>(index), reinterpret_cast<LPARAM>(&item));
+
+		return item;
+	}
+
+	bool BaseTabControl::removeTab(size_t index)
+	{
+		return SendMessageW(handle, TCM_DELETEITEM, static_cast<WPARAM>(index), NULL);
+	}
+
+	bool BaseTabControl::clear()
+	{
+		return SendMessageW(handle, TCM_DELETEALLITEMS, NULL, NULL);
 	}
 
 	size_t BaseTabControl::size() const
--- GUIFramework/src/BaseComponents/StandardComponents/BaseTabControl.h
+++ GUIFramework/src/BaseComponents/StandardComponents/BaseTabControl.h
@@ -10,24 +10,57 @@ namespace gui_framework
 	protected:
 		HIMAGELIST imageList;
 		std::unordered_map<std::wstring, uint16_t> images;
+		uint16_t iconWidth;
+		uint16_t iconHeight;
 
 	public:
-		BaseTabControl(const std::wstring& tabControlName, const utility::ComponentSettings& settings, BaseComponent* parent);
+		BaseTabControl(const std::wstring& tabControlName, const utility::ComponentSettings& settings, uint16_t iconsWidth, uint16_t iconsHeight, BaseComponent* parent);
 
-		virtual LRESULT append(const std::wstring& text) final;
+		virtual LRESULT appendText(const std::wstring& text) final;
+
+		virtual LRESULT insertText(size_t index, const std::wstring& text) final;
 
 		/// @brief 
 		/// @param pathToImage 
 		/// @return
 		/// @exception gui_framework::exceptions::FileDoesNotExist
-		virtual LRESULT append(const std::filesystem::path& pathToImage) final;
+		virtual LRESULT appendImage(const std::filesystem::path& pathToImage) final;
+
+		/// @brief 
+		/// @param pathToImage 
+		/// @param index 
+		/// @return 
+		/// @exception gui_framework::exceptions::FileDoesNotExist
+		virtual LRESULT insertImage(size_t index, const std::filesystem::path& pathToImage) final;
 
 		/// @brief 
 		/// @param text 
 		/// @param pathToImage 
 		/// @return
 		/// @exception gui_framework::exceptions::FileDoesNotExist 
-		virtual LRESULT append(const std::wstring& text, const std::filesystem::path& pathToImage) final;
+		virtual LRESULT appendTextAndImage(const std::wstring& text, const std::filesystem::path& pathToImage) final;
+
+		/// @brief 
+		/// @param text 
+		/// @param pathToImage 
+		/// @param index 
+		/// @return 
+		/// @exception gui_framework::exceptions::FileDoesNotExist 
+		virtual LRESULT insertTextAndImage(size_t index, const std::wstring& text, const std::filesystem::path& pathToImage) final;
+
+		/// @brief 
+		/// @param index 
+		/// @param text 
+		/// @param pathToImage 
+		/// @return 
+		/// @exception gui_framework::exceptions::FileDoesNotExist 
+		virtual bool setItem(size_t index, const std::wstring& text = L"", const std::filesystem::path& pathToImage = L"") final;
+
+		virtual TCITEMW getItem(size_t index) final;
+
+		virtual bool removeTab(size_t index) final;
+
+		virtual bool clear() final;
 
 		virtual size_t size() const final;
 
