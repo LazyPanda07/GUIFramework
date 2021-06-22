6fbe0edfdc3cfea4ee1caa734339e7cb01780843
LazyPanda07 <semengricenko@gmail.com>
2021-06-22 22:33:55 +0500

    Refactor

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -18,10 +18,8 @@ using namespace std;
 
 CREATE_DEFAULT_WINDOW_FUNCTION(main)
 
-int main(int argc, char** argv)
+void test()
 {
-	gui_framework::GUIFramework::get();
-
 	using namespace gui_framework;
 
 	utility::ComponentSettings settings(WS_BORDER, 300, 200, 800, 600);
@@ -36,55 +34,40 @@ int main(int argc, char** argv)
 
 		ptr->setLargeIcon(R"(assets\icon.ico)");
 
-		Button* button = new Button(L"Button", L"Текст", 0, 0, ptr, [](WPARAM, LPARAM) { cout << "Click" << endl; return 0; });
-		
-		EditControl* editControl = new EditControl(L"Edit", 0, 20, ptr);
-		
-		StaticControl* staticControl = new StaticControl(L"Static", L"Текст", 0, 40, ptr);
-		
-		RichEdit* richEdit = new RichEdit(L"Rich", 0, 80, 200, 40, ptr, true);
-		
 		BaseComboBox* simpleComboBox = new SimpleComboBox(L"ComboBox", 200, 0, 200, 80, ptr);
-		
+
 		BaseComboBox* dropDownComboBox = new DropDownComboBox(L"DropDownBox", 200, 150, 200, 80, ptr);
-		
+
 		BaseComboBox* dropDownListComboBox = new DropDownListComboBox(L"DropDownListBox", 200, 300, 200, 80, ptr);
-		
-		ListBox* listBox = new ListBox(L"ListBox", 400, 0, 200, 80, ptr);
-		
+
 		simpleComboBox->addValue(L"First combo box");
 		simpleComboBox->addValue(L"Second combo box");
-		
+
 		dropDownComboBox->addValue(L"First drop down combo box");
 		dropDownComboBox->addValue(L"Second drop down combo box");
-		
+
 		dropDownListComboBox->addValue(L"First drop down list combo box");
 		dropDownListComboBox->addValue(L"Second drop down list combo box");
-		
-		listBox->addValue(L"First list box");
-		listBox->addValue(L"Second list box");
-		
-		vector<BaseComponent*> components =
-		{
-			button,
-			editControl,
-			staticControl,
-			richEdit,
-			listBox
-		};
-		
-		srand(time(nullptr));
-		
-		for (const auto& i : components)
-		{
-			i->setBackgroundColor(rand() % 255, rand() % 255, rand() % 255);
-		}
 
 		holder.runMainLoop();
 	}
 	catch (const std::exception& e)
 	{
 		cout << e.what() << endl;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	gui_framework::GUIFramework::get();
+
+	thread(test).detach();
+
+	string s;
+
+	while (cin >> s)
+	{
+
 	}
 
 	return 0;
