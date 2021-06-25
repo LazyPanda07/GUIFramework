d7d3286997de0fb0c3ff45effbf0a4275d2e9ac0
LazyPanda07 <semengricenko@gmail.com>
2021-06-25 21:39:02 +0500

    Added ImagesHolder

---
+++ GUIFramework/src/Utility/ImagesHolder.cpp
@@ -0,0 +1,88 @@
+#include "pch.h"
+#include "ImagesHolder.h"
+
+using namespace std;
+
+namespace gui_framework
+{
+	namespace interfaces
+	{
+		ImagesHolder::imageData::imageData(uint16_t index, imageType type) :
+			index(index),
+			type(type)
+		{
+
+		}
+
+		ImagesHolder::ImagesHolder(uint16_t imagesWidth, uint16_t imagesHeight, size_t count) :
+			imageList(ImageList_Create(imagesWidth, imagesHeight, ILC_COLOR32, 0, count)),
+			imagesWidth(imagesWidth),
+			imagesHeight(imagesHeight)
+		{
+
+		}
+
+		uint16_t ImagesHolder::addImage(const filesystem::path& pathToImage, imageType type)
+		{
+			HBITMAP image = static_cast<HBITMAP>(LoadImageW(nullptr, pathToImage.wstring().data(), static_cast<uint32_t>(type), imagesWidth, imagesHeight, LR_LOADFROMFILE));
+
+			return images.emplace(pathToImage, imageData(ImageList_Add(imageList, image, NULL), type)).first->second.index;
+		}
+
+		void ImagesHolder::removeImage(const filesystem::path& pathToImage)
+		{
+			images.erase(pathToImage);
+		}
+
+		void ImagesHolder::removeImage(uint16_t imageIndex)
+		{
+			for (const auto& [key, value] : images)
+			{
+				if (value.index == imageIndex)
+				{
+					images.erase(key);
+
+					break;
+				}
+			}
+		}
+
+		uint16_t ImagesHolder::getImageIndex(const filesystem::path& pathToImage) const
+		{
+			return images.at(pathToImage).index;
+		}
+
+		ImagesHolder::imageType ImagesHolder::getImageType(const filesystem::path& pathToImage) const
+		{
+			return images.at(pathToImage).type;
+		}
+
+		ImagesHolder::imageType ImagesHolder::getImageType(uint16_t imageIndex) const
+		{
+			for (const auto& [key, value] : images)
+			{
+				if (value.index == imageIndex)
+				{
+					return value.type;
+				}
+			}
+
+			throw out_of_range(format("Can't find imageType at {} index", imageIndex));
+		}
+
+		HIMAGELIST ImagesHolder::getImageList() const
+		{
+			return imageList;
+		}
+
+		uint16_t ImagesHolder::operator [] (const filesystem::path& pathToImage) const
+		{
+			return images.at(pathToImage).index;
+		}
+
+		ImagesHolder::~ImagesHolder()
+		{
+			ImageList_Destroy(imageList);
+		}
+	}
+}
---
+++ GUIFramework/src/Utility/ImagesHolder.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "pch.h"
+
+namespace gui_framework
+{
+	namespace interfaces
+	{
+		class ImagesHolder final
+		{
+		public:
+			enum class imageType : uint32_t
+			{
+				bitmap,
+				icon,
+				cursor
+			};
+
+		private:
+			struct imageData
+			{
+				uint16_t index;
+				imageType type;
+
+				imageData(uint16_t index, imageType type);
+
+				imageData(const imageData& other) = default;
+
+				imageData(imageData&& other) noexcept = default;
+
+				imageData& operator = (const imageData& other) = default;
+
+				imageData& operator = (imageData&& other) noexcept = default;
+
+				~imageData() = default;
+			};
+
+		private:
+			HIMAGELIST imageList;
+			std::unordered_map<std::wstring, imageData> images;
+			uint16_t imagesWidth;
+			uint16_t imagesHeight;
+
+		public:
+			ImagesHolder(uint16_t imagesWidth, uint16_t imagesHeight, size_t count = standard_sizes::defaultImagesCount);
+
+			/// @brief 
+			/// @param pathToImage 
+			/// @return Index of newly added image
+			uint16_t addImage(const std::filesystem::path& pathToImage, imageType type);
+
+			void removeImage(const std::filesystem::path& pathToImage);
+
+			void removeImage(uint16_t imageIndex);
+
+			/// @brief 
+			/// @param pathToImage 
+			/// @return
+			/// @exception std::out_of_range 
+			uint16_t getImageIndex(const std::filesystem::path& pathToImage) const;
+
+			imageType getImageType(const std::filesystem::path& pathToImage) const;
+
+			/// @brief 
+			/// @param imageIndex 
+			/// @return
+			/// @exception std::out_of_range 
+			imageType getImageType(uint16_t imageIndex) const;
+
+			HIMAGELIST getImageList() const;
+
+			/// @brief 
+			/// @param pathToImage 
+			/// @return 
+			/// @exception std::out_of_range
+			uint16_t operator [] (const std::filesystem::path& pathToImage) const;
+
+			~ImagesHolder();
+		};
+	}
+}
