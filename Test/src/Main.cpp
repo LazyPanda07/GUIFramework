3c4c92ec78deef0b5ee9199d926d0c9a1daf4907
LazyPanda07 <semengricenko@gmail.com>
2021-07-03 19:11:35 +0500

    Refactor

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -2,7 +2,7 @@
 
 #include "GUIFramework.h"
 #include "WindowHolder.h"
-#include "BaseComponents/StandardComponents/ListView/BaseTextListView.h"
+#include "BaseComponents/StandardComponents/ListView/BaseIconListView.h"
 #include "Composites/SeparateWindow.h"
 
 #pragma comment (lib, "GUIFramework.lib")
@@ -22,21 +22,13 @@ void test()
 		WindowHolder holder(make_unique<SeparateWindow>(L"MainWindow", L"Главное окно", settings, "main"));
 
 		SeparateWindow* ptr = dynamic_cast<SeparateWindow*>(holder.get());
-		BaseTextListView* list = new BaseTextListView(L"List", utility::ComponentSettings(WS_BORDER, 0, 0, 200, 100), ptr);
+		BaseIconListView* list = new BaseIconListView(L"List", utility::ComponentSettings(WS_BORDER, 0, 0, 200, 100), ptr, utility::iconListViewType::icon, 32, 32);
 
-		LVITEMW item = {};
+		list->addIconItem(R"(assets\icon.ico)");
 
-		item.mask = LVIF_TEXT;
-		item.iItem = 0;
-		item.pszText = const_cast<wchar_t*>(L"First");
-		item.cchTextMax = 6;
+		list->addIconItem(R"(assets\icon.ico)");
 
-		SendMessageW(list->getHandle(), LVM_ISITEMVISIBLE, SendMessageW(list->getHandle(), LVM_INSERTITEM, NULL, reinterpret_cast<LPARAM>(&item)), NULL);
-
-		item.pszText = const_cast<wchar_t*>(L"Second");
-		item.cchTextMax = 7;
-
-		SendMessageW(list->getHandle(), LVM_ISITEMVISIBLE, SendMessageW(list->getHandle(), LVM_INSERTITEM, NULL, reinterpret_cast<LPARAM>(&item)), NULL);
+		list->addIconItem(R"(assets\icon.ico)");
 
 		ptr->setExitMode(BaseComponent::exitMode::quit);
 
