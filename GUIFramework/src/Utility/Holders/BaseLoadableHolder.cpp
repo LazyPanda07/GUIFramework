75ead77d22d9254a747c9b1b6bc045c91d8a61fb
LazyPanda07 <semengricenko@gmail.com>
2021-07-03 17:06:27 +0500

    Added BaseLoadableHolder

---
+++ GUIFramework/src/Utility/Holders/BaseLoadableHolder.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "BaseLoadableHolder.h"
+
+using namespace std;
+
+namespace gui_framework
+{
+	namespace utility
+	{
+
+	}
+}
---
+++ GUIFramework/src/Utility/Holders/BaseLoadableHolder.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "pch.h"
+
+namespace gui_framework
+{
+	namespace utility
+	{
+		class GUI_FRAMEWORK_API BaseLoadableHolder
+		{
+
+		};
+	}
+}
