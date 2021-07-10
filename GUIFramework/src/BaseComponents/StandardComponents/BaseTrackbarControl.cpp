f6938cef136abd6ee53c8dd6256e1d45db3293b2
LazyPanda07 <semengricenko@gmail.com>
2021-07-11 01:12:27 +0500

    Added functionality to BaseTrackbarControl

--- GUIFramework/src/BaseComponents/StandardComponents/BaseTrackbarControl.cpp
+++ GUIFramework/src/BaseComponents/StandardComponents/BaseTrackbarControl.cpp
@@ -16,4 +16,69 @@ namespace gui_framework
 	{
 
 	}
+
+	void BaseTrackbarControl::clearSelection()
+	{
+		SendMessageW(handle, TBM_CLEARSEL, true, NULL);
+	}
+
+	LRESULT BaseTrackbarControl::size() const
+	{
+		return SendMessageW(handle, TBM_GETLINESIZE, NULL, NULL);
+	}
+
+	void BaseTrackbarControl::setMinRange(int min)
+	{
+		SendMessageW(handle, TBM_SETRANGEMIN, true, min);
+	}
+
+	void BaseTrackbarControl::setMaxRange(int max)
+	{
+		SendMessageW(handle, TBM_SETRANGEMAX, true, max);
+	}
+
+	void BaseTrackbarControl::setSelectionStart(int startPosition)
+	{
+		SendMessageW(handle, TBM_SETSELSTART, true, startPosition);
+	}
+
+	void BaseTrackbarControl::setSelectionEnd(int endPosition)
+	{
+		SendMessageW(handle, TBM_SETSELEND, true, endPosition);
+	}
+
+	LRESULT BaseTrackbarControl::getTicksCount() const
+	{
+		return SendMessageW(handle, TBM_GETNUMTICS, NULL, NULL);
+	}
+
+	LRESULT BaseTrackbarControl::getPosition() const
+	{
+		return SendMessageW(handle, TBM_GETPOS, NULL, NULL);
+	}
+
+	LRESULT BaseTrackbarControl::getTickPosition() const
+	{
+		return SendMessageW(handle, TBM_GETTICPOS, NULL, NULL);
+	}
+
+	LRESULT BaseTrackbarControl::getMinRange() const
+	{
+		return SendMessageW(handle, TBM_GETRANGEMIN, NULL, NULL);
+	}
+
+	LRESULT BaseTrackbarControl::getMaxRange() const
+	{
+		return SendMessageW(handle, TBM_GETRANGEMAX, NULL, NULL);
+	}
+
+	LRESULT BaseTrackbarControl::getSelectionStart() const
+	{
+		return SendMessageW(handle, TBM_GETSELSTART, NULL, NULL);
+	}
+
+	LRESULT BaseTrackbarControl::getSelectionEnd() const
+	{
+		return SendMessageW(handle, TBM_GETSELEND, NULL, NULL);
+	}
 }
--- GUIFramework/src/BaseComponents/StandardComponents/BaseTrackbarControl.h
+++ GUIFramework/src/BaseComponents/StandardComponents/BaseTrackbarControl.h
@@ -9,6 +9,32 @@ namespace gui_framework
 	public:
 		BaseTrackbarControl(const std::wstring& trackbarName, const utility::ComponentSettings& settings, BaseComponent* parent);
 
+		virtual void clearSelection() final;
+
+		virtual LRESULT size() const final;
+
+		virtual void setMinRange(int min) final;
+
+		virtual void setMaxRange(int max) final;
+
+		virtual void setSelectionStart(int startPosition) final;
+
+		virtual void setSelectionEnd(int endPosition) final;
+
+		virtual LRESULT getTicksCount() const final;
+
+		virtual LRESULT getPosition() const final;
+
+		virtual LRESULT getTickPosition() const final;
+
+		virtual LRESULT getMinRange() const final;
+
+		virtual LRESULT getMaxRange() const final;
+
+		virtual LRESULT getSelectionStart() const final;
+
+		virtual LRESULT getSelectionEnd() const final;
+
 		virtual ~BaseTrackbarControl() = default;
 	};
 }
