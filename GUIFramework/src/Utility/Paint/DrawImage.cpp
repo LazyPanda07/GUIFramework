dc91e8f200998319db3dd523d50462bb989325d0
LazyPanda07 <semengricenko@gmail.com>
2021-07-11 18:22:55 +0500

    C

--- GUIFramework/src/Utility/Paint/DrawImage.cpp
+++ GUIFramework/src/Utility/Paint/DrawImage.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 template<typename T>
-bool drawImageImplementation(const gui_framework::BaseWindow* window, const gui_framework::utility::BaseLoadableHolder& holder, int x, int y, const T& index);
+bool drawImageImplementation(const gui_framework::BaseWindow* window, const gui_framework::utility::BaseLoadableHolder& holder, int x, int y, const T& index, uint16_t width, uint16_t height);
 
 namespace gui_framework
 {
@@ -12,21 +12,21 @@ namespace gui_framework
 	{
 		namespace paint
 		{
-			bool drawImage(const BaseWindow* window, const BaseLoadableHolder& holder, int x, int y, uint16_t index)
+			bool drawImage(const BaseWindow* window, const BaseLoadableHolder& holder, int x, int y, uint16_t index, uint16_t width, uint16_t height)
 			{
-				return drawImageImplementation(window, holder, x, y, index);
+				return drawImageImplementation(window, holder, x, y, index, width, height);
 			}
 
-			bool drawImage(const BaseWindow* window, const BaseLoadableHolder& holder, int x, int y, const std::filesystem::path& pathToImage)
+			bool drawImage(const BaseWindow* window, const BaseLoadableHolder& holder, int x, int y, const std::filesystem::path& pathToImage, uint16_t width, uint16_t height)
 			{
-				return drawImageImplementation(window, holder, x, y, pathToImage);
+				return drawImageImplementation(window, holder, x, y, pathToImage, width, height);
 			}
 		}
 	}
 }
 
 template<typename T>
-bool drawImageImplementation(const gui_framework::BaseWindow* window, const gui_framework::utility::BaseLoadableHolder& holder, int x, int y, const T& index)
+bool drawImageImplementation(const gui_framework::BaseWindow* window, const gui_framework::utility::BaseLoadableHolder& holder, int x, int y, const T& index, uint16_t width, uint16_t height)
 {
 	PAINTSTRUCT paint = {};
 	HDC deviceContext = BeginPaint(window->getHandle(), &paint);
@@ -34,13 +34,14 @@ bool drawImageImplementation(const gui_framework::BaseWindow* window, const gui_
 
 	if constexpr (is_same_v<T, uint16_t>)
 	{
-		result = ImageList_Draw(holder.getImageList(), index, deviceContext, x, y, ILD_TRANSPARENT);
+		result = ImageList_DrawEx(holder.getImageList(), index, deviceContext, x, y, width, height, CLR_DEFAULT, CLR_DEFAULT, ILD_TRANSPARENT);
 	}
 	else
 	{
-		result = ImageList_Draw(holder.getImageList(), holder.getImageIndex(index), deviceContext, x, y, ILD_TRANSPARENT);
+		result = ImageList_DrawEx(holder.getImageList(), holder.getImageIndex(index), deviceContext, x, y, width, height, CLR_DEFAULT, CLR_DEFAULT, ILD_TRANSPARENT);
 	}
-	
+
+	ReleaseDC(window->getHandle(), deviceContext);
 	EndPaint(window->getHandle(), &paint);
 
 	return result;
--- GUIFramework/src/Utility/Paint/DrawImage.h
+++ GUIFramework/src/Utility/Paint/DrawImage.h
@@ -15,9 +15,11 @@ namespace gui_framework
 			/// @param x 
 			/// @param y 
 			/// @param index Internal index of image
+			/// @param width Zero means take entire image width
+			/// @param height Zero means take entire image height
 			/// @return 
 			/// @exception std::out_of_range Wrong index
-			GUI_FRAMEWORK_API_FUNCTION bool drawImage(const BaseWindow* window, const BaseLoadableHolder& holder, int x, int y, uint16_t index);
+			GUI_FRAMEWORK_API_FUNCTION bool drawImage(const BaseWindow* window, const BaseLoadableHolder& holder, int x, int y, uint16_t index, uint16_t width = 0, uint16_t height = 0);
 
 			/// @brief Draw image from holder
 			/// @param window 
@@ -25,9 +27,11 @@ namespace gui_framework
 			/// @param x 
 			/// @param y 
 			/// @param pathToImage 
+			/// @param width Zero means take entire image width
+			/// @param height Zero means take entire image height
 			/// @return 
 			/// @exception std::out_of_range Wrong pathToImage
-			GUI_FRAMEWORK_API_FUNCTION bool drawImage(const BaseWindow* window, const BaseLoadableHolder& holder, int x, int y, const std::filesystem::path& pathToImage);
+			GUI_FRAMEWORK_API_FUNCTION bool drawImage(const BaseWindow* window, const BaseLoadableHolder& holder, int x, int y, const std::filesystem::path& pathToImage, uint16_t width = 0, uint16_t height = 0);
 		}
 	}
 }
