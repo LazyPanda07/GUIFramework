0d7a8074a2734abfd30b2ce8b49c025a0c8e4632
LazyPanda07 <semengricenko@gmail.com>
2021-07-26 00:50:56 +0500

    Refactor

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 #include "GUIFramework.h"
+#include "Components/StaticControl.h"
+#include "Components/Buttons/Button.h"
+#include "Composites/ChildWindow.h"
 #include "Utility/Holders/WindowHolder.h"
 #include "Composites/SeparateWindow.h"
 
@@ -9,6 +12,8 @@
 using namespace std;
 
 CREATE_DEFAULT_WINDOW_FUNCTION(main)
+
+CREATE_DEFAULT_WINDOW_FUNCTION(child)
 
 void test()
 {
@@ -20,6 +25,12 @@ void test()
 	{
 		WindowHolder holder(make_unique<SeparateWindow>(L"MainWindow", L"Главное окно", settings, "main"));
 		SeparateWindow* ptr = dynamic_cast<SeparateWindow*>(holder.get());
+		new StaticControl(L"Текст", L"Текст в окне", 50, 50, ptr);
+		new Button(L"Pure button", L"Some text on button", 500, 20, ptr, []() { cout << "Nice" << endl; });
+
+		ChildWindow* window = new ChildWindow(L"Child class", L"Nested class", utility::ComponentSettings(50, 300, 200, 200), ptr, "child");
+
+		cout << *ptr << endl;
 
 		ptr->setExitMode(BaseComponent::exitMode::quit);
 
@@ -36,6 +47,8 @@ void test()
 int main(int argc, char** argv)
 {
 	gui_framework::GUIFramework::get();
+
+	SetConsoleOutputCP(CP_UTF8);
 
 	thread(test).detach();
 
