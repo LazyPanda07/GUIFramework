507661c3eb32444c73de329f04a149964c4f3eaf
LazyPanda07 <semengricenko@gmail.com>
2021-08-06 18:26:37 +0500

    Refactor

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -6,6 +6,8 @@
 #include "Composites/ChildWindow.h"
 #include "Utility/Holders/WindowHolder.h"
 #include "Composites/SeparateWindow.h"
+#include "MenuItems/MenuItem.h"
+#include "MenuItems/DropDownMenuItem.h"
 
 #pragma comment (lib, "GUIFramework.lib")
 
@@ -14,6 +16,21 @@ using namespace std;
 CREATE_DEFAULT_WINDOW_FUNCTION(main)
 
 CREATE_DEFAULT_WINDOW_FUNCTION(child)
+
+void createMenus(gui_framework::SeparateWindow* ptr)
+{
+	using namespace gui_framework;
+
+	unique_ptr<Menu>& menu = ptr->createMainMenu(L"MainMenu");
+
+	menu->addMenuItem(make_unique<MenuItem>(L"First", []() { cout << "First" << endl; }));
+
+	Menu& popup = ptr->addPopupMenu(L"Second");
+
+	popup.addMenuItem(make_unique<MenuItem>(L"Inside", []() { cout << "Inside" << endl; }));
+
+	menu->addMenuItem(make_unique<DropDownMenuItem>(L"Popup", popup.getHandle()));
+}
 
 void test()
 {
@@ -26,9 +43,8 @@ void test()
 		WindowHolder holder(make_unique<SeparateWindow>(L"MainWindow", L"Главное окно", settings, "main"));
 		SeparateWindow* ptr = dynamic_cast<SeparateWindow*>(holder.get());
 		new StaticControl(L"Текст", L"Текст в окне", 50, 50, ptr);
-		new Button(L"Pure button", L"Some text on button", 500, 20, ptr, []() { cout << "Nice" << endl; });
 
-		ChildWindow* window = new ChildWindow(L"Child class", L"Nested class", utility::ComponentSettings(50, 300, 200, 200), ptr, "child");
+		createMenus(ptr);
 
 		cout << *ptr << endl;
 
