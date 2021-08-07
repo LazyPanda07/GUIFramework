3d511dbe46afd919c1815ee4355010374c561cfa
LazyPanda07 <semengricenko@gmail.com>
2021-08-07 17:26:30 +0500

    Refactor

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -3,11 +3,10 @@
 #include "GUIFramework.h"
 #include "Components/StaticControl.h"
 #include "Components/Buttons/Button.h"
+#include "Components/ComboBoxes/DropDownListComboBox.h"
 #include "Composites/ChildWindow.h"
 #include "Utility/Holders/WindowHolder.h"
 #include "Composites/SeparateWindow.h"
-#include "MenuItems/MenuItem.h"
-#include "MenuItems/DropDownMenuItem.h"
 
 #pragma comment (lib, "GUIFramework.lib")
 
@@ -16,21 +15,6 @@ using namespace std;
 CREATE_DEFAULT_WINDOW_FUNCTION(main)
 
 CREATE_DEFAULT_WINDOW_FUNCTION(child)
-
-void createMenus(gui_framework::SeparateWindow* ptr)
-{
-	using namespace gui_framework;
-
-	unique_ptr<Menu>& menu = ptr->createMainMenu(L"MainMenu");
-
-	menu->addMenuItem(make_unique<MenuItem>(L"First", []() { cout << "First" << endl; }));
-
-	Menu& popup = ptr->addPopupMenu(L"Second");
-
-	popup.addMenuItem(make_unique<MenuItem>(L"Inside", []() { cout << "Inside" << endl; }));
-
-	menu->addMenuItem(make_unique<DropDownMenuItem>(L"Popup", popup.getHandle()));
-}
 
 void test()
 {
@@ -42,11 +26,14 @@ void test()
 	{
 		WindowHolder holder(make_unique<SeparateWindow>(L"MainWindow", L"Главное окно", settings, "main"));
 		SeparateWindow* ptr = dynamic_cast<SeparateWindow*>(holder.get());
-		new StaticControl(L"Текст", L"Текст в окне", 50, 50, ptr);
+		DropDownListComboBox* list = new DropDownListComboBox(L"Drop", utility::ComponentSettings(0, 0, 400, 20), ptr);
 
-		createMenus(ptr);
+		list->setBlockResize(true);
 
-		cout << *ptr << endl;
+		list->addValue(L"First");
+		list->addValue(L"Second");
+		list->addValue(L"Third");
+		list->addValue(L"Fourth");
 
 		ptr->setExitMode(BaseComponent::exitMode::quit);
 
