d754bf62a4f07d01ca7c91637ce9f512763b5a16
LazyPanda07 <semengricenko@gmail.com>
2021-08-11 19:03:08 +0500

    Added module name in functions serialization

--- GUIFramework/src/BaseComponents/StandardComponents/BaseComboBox.cpp
+++ GUIFramework/src/BaseComponents/StandardComponents/BaseComboBox.cpp
@@ -363,6 +363,8 @@ namespace gui_framework
 
 		current->data.push_back({ "functionName"s, functionName });
 
+		current->data.push_back({ "moduleName"s, moduleName });
+
 		current->data.push_back({ "pathToModule"s, GUIFramework::get().getModulesPaths().at(moduleName) });
 
 		return builder;
