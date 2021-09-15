a025bfd6bfa8c0d3719554c5cca9f318e946d28c
LazyPanda07 <semengricenko@gmail.com>
2021-09-16 03:55:22 +0500

    Added ChildWindowDeserializer, fixes

---
+++ GUIFramework/src/Deserialization/Deserializers/Composites/ChildWindowDeserializer.cpp
@@ -0,0 +1,14 @@
+#include "ChildWindowDeserializer.h"
+
+using namespace std;
+
+namespace gui_framework
+{
+	namespace deserializers
+	{
+		BaseComponent* ChildWindowDeserializer::deserialize(const string& componentName, const json::utility::objectSmartPointer<json::utility::jsonObject>& description, BaseComposite* parent) const
+		{
+			return nullptr;
+		}
+	}
+}
