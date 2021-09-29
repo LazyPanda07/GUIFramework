8f203fbfd66637ddc8cc1cb8317f6c99c4aaf4bf
LazyPanda07 <semengricenko@gmail.com>
2021-09-29 17:21:04 +0500

    Refactor

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -27,24 +27,26 @@ void standard()
 	{
 		WindowHolder holder(make_unique<SeparateWindow>(L"MainWindow", L"Главное окно", settings, "main"));
 		SeparateWindow* ptr = dynamic_cast<SeparateWindow*>(holder.get());
-		ChildWindow* child = new ChildWindow(L"ChildWindow", L"Child window name", utility::ComponentSettings(600, 400, 200, 200), ptr, "child");
-		ListTextIconListView* list = new ListTextIconListView(L"List", utility::ComponentSettings(0, 0, 300, 200), ptr, utility::iconListViewType::smallIcon);
-		RichEdit* richEdit = new RichEdit(L"RichEdit", utility::ComponentSettings(300, 0, 300, 200), ptr, true);
-		new StaticControl(L"Text", L"Текст внутри ChildWindow", 0, 0, child);
-		
-		richEdit->setAutoURLDetect(true);
-		
-		richEdit->addUrlDetectEvent(BaseRichEdit::urlDetectEvent::mouseMove, "testEdit", "callbacks");
-		
-		list->addTextIconItem(L"First", "assets/icon.ico");
-		
-		list->addTextIconItem(L"Second", "assets/another_icon.ico");
-
+		// ChildWindow* child = new ChildWindow(L"ChildWindow", L"Child window name", utility::ComponentSettings(600, 400, 200, 200), ptr, "child");
+		// ListTextIconListView* list = new ListTextIconListView(L"List", utility::ComponentSettings(0, 0, 300, 200), ptr, utility::iconListViewType::smallIcon);
+		// RichEdit* richEdit = new RichEdit(L"RichEdit", utility::ComponentSettings(300, 0, 300, 200), ptr, true);
+		// new StaticControl(L"Text", L"Текст внутри ChildWindow", 0, 0, child);
+		// 
+		// richEdit->setAutoURLDetect(true);
+		// 
+		// richEdit->addUrlDetectEvent(BaseRichEdit::urlDetectEvent::mouseMove, "testEdit", "callbacks");
+		// 
+		// list->addTextIconItem(L"First", "assets/icon.ico");
+		// 
+		// list->addTextIconItem(L"Second", "assets/another_icon.ico");
+		// 
 		ptr->setExitMode(BaseComponent::exitMode::quit);
-
+		
 		ptr->setLargeIcon(R"(assets\icon.ico)");
+		// 
+		// thread([ptr]() { this_thread::sleep_for(2s); ofstream("test.json") << *ptr << endl; }).detach();
 
-		thread([ptr]() { this_thread::sleep_for(2s); ofstream("test.json") << *ptr << endl; }).detach();
+		GUIFramework::get().registerHotkey(0x31, [ptr]() { ptr->setBackgroundColor(rand() % 256, rand() ^ 256, rand() % 256); });
 
 		holder.runMainLoop();
 	}
