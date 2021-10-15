409573d1e05852f0f09e8318d555a8388ae1acb2
LazyPanda07 <semengricenko@gmail.com>
2021-10-15 21:06:57 +0500

    Added parsers

--- GUIFramework/src/Deserialization/Deserializers/Composites/ChildWindowDeserializer.cpp
+++ GUIFramework/src/Deserialization/Deserializers/Composites/ChildWindowDeserializer.cpp
@@ -1,6 +1,8 @@
 #include "headers.h"
 #include "ChildWindowDeserializer.h"
 
+#include "Deserialization/Deserializers/Parsers/BaseCompositeParser.h"
+
 using namespace std;
 
 namespace gui_framework
@@ -9,7 +11,42 @@ namespace gui_framework
 	{
 		BaseComponent* ChildWindowDeserializer::deserialize(const string& componentName, const json::utility::objectSmartPointer<json::utility::jsonObject>& description, BaseComposite* parent) const
 		{
-			return nullptr;
+			using json::utility::objectSmartPointer;
+			using json::utility::jsonObject;
+
+			parsers::BaseCompositeParser parser;
+
+			parser.parse(description);
+
+			ChildWindow* result = nullptr;
+			uint32_t codepage = interfaces::ISerializable::getCodepage();
+			const unique_ptr<utility::BaseComponentCreator>& creator = GUIFramework::get().getCreators().at(utility::getTypeHash<ChildWindow>());
+			utility::AdditionalCreationData<ChildWindow> creationData(parser.className, parser.windowFunctionName);
+			const auto& children = description->getArray("children");
+
+			result = static_cast<ChildWindow*>(creator->create(utility::to_wstring(componentName, codepage), parser.settings, creationData.getData(), nullptr));
+
+			result->setBackgroundColor(parser.backgroundColor[0], parser.backgroundColor[1], parser.backgroundColor[2]);
+
+			result->setTextColor(parser.textColor[0], parser.textColor[1], parser.textColor[2]);
+
+			result->setExitMode(parser.mode);
+
+			result->setLargeIcon(parser.pathToLargeIcon);
+
+			result->setSmallIcon(parser.pathToSmallIcon);
+
+			for (const auto& i : children)
+			{
+				const auto& [componentName, data] = get<objectSmartPointer<jsonObject>>(i->data.front().second)->data.front();
+				const auto& description = get<objectSmartPointer<jsonObject>>(data);
+
+				const unique_ptr<interfaces::IDeserializer>& deserializer = GUIFramework::get().getDeserializers().at(description->getUnsignedInt("hash"));
+
+				deserializer->deserialize(componentName, description, result);
+			}
+
+			return result;
 		}
 	}
 }
