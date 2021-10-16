d7d22f1aeb323cc00457275a3cfe955a0e899808
LazyPanda07 <semengricenko@gmail.com>
2021-10-17 02:01:10 +0500

    Added StaticControlDeserializer

---
+++ GUIFramework/src/Deserialization/Deserializers/Components/StaticControlDeserializer.cpp
@@ -0,0 +1,36 @@
+#include "StaticControlDeserializer.h"
+
+#include "Deserialization/Deserializers/Parsers/BaseComponentParser.h"
+
+using namespace std;
+
+namespace gui_framework
+{
+	namespace deserializers
+	{
+		BaseComponent* StaticControlDeserializer::deserialize(const string& componentName, const json::utility::objectSmartPointer<json::utility::jsonObject>& description, BaseComposite* parent) const
+		{
+			using json::utility::objectSmartPointer;
+			using json::utility::jsonObject;
+
+			parsers::BaseComponentParser parser;
+
+			parser.parse(description);
+
+			StaticControl* result = nullptr;
+			uint32_t codepage = interfaces::ISerializable::getCodepage();
+			const unique_ptr<utility::BaseComponentCreator>& creator = GUIFramework::get().getCreators().at(utility::getTypeHash<StaticControl>());
+			utility::AdditionalCreationData<StaticControl> creationData(utility::to_wstring(description->getString("text"), codepage));
+
+			result = static_cast<StaticControl*>(creator->create(utility::to_wstring(componentName, codepage), parser.settings, creationData.getData(), parent));
+
+			result->setBackgroundColor(parser.backgroundColor[0], parser.backgroundColor[1], parser.backgroundColor[2]);
+
+			result->setTextColor(parser.textColor[0], parser.textColor[1], parser.textColor[2]);
+
+			result->setStyles(parser.styles);
+
+			return result;
+		}
+	}
+}
