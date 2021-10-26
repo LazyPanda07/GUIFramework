9eeb3a5efd48512924c1fb2dfc714d97ddf30054
LazyPanda07 <semengricenko@gmail.com>
2021-10-26 22:11:02 +0500

    Added comments

--- GUIFramework/src/Exceptions/Deserialization/WrongMenuTypeException.h
+++ GUIFramework/src/Exceptions/Deserialization/WrongMenuTypeException.h
@@ -8,6 +8,7 @@ namespace gui_framework
 	{
 		namespace deserialization
 		{
+			/// @brief Throws by SeparateWindowDeserializer during deserialize if JSON has wrong itemType value
 			class GUI_FRAMEWORK_API WrongMenuTypeException : public BaseDeserializationException
 			{
 			public:
