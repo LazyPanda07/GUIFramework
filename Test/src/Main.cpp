6577dea2cecfd2c537e68aa692ee4b5d72ddda2d
LazyPanda07 <semengricenko@gmail.com>
2022-03-01 20:43:02 +0500

    Refactor

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -6,6 +6,7 @@
 #include "MenuItems/DropDownMenuItem.h"
 #include "ComponentsHeader.h"
 #include "CompositesHeader.h"
+#include "AdditionalCreationDataHeader.h"
 
 #pragma comment (lib, "GUIFramework.lib")
 
@@ -13,7 +14,7 @@ using namespace std;
 
 CREATE_DEFAULT_WINDOW_FUNCTION(main)
 
-CREATE_DEFAULT_WINDOW_FUNCTION(child)
+CREATE_DEFAULT_WINDOW_FUNCTION(dialog)
 
 void standard()
 {
@@ -25,13 +26,29 @@ void standard()
 	{
 		WindowHolder holder(make_unique<SeparateWindow>(L"MainWindow", L"Главное окно", settings, "main"));
 		SeparateWindow* ptr = dynamic_cast<SeparateWindow*>(holder.get());
-		ChildWindow* child = new ChildWindow(L"ChildWindow", L"Child window name", utility::ComponentSettings(600, 400, 200, 200), ptr, "child");
-
-		ptr->setOnClose([ptr]() { return DialogBox::createMessageBox(L"Close?", L"Closing", DialogBox::messageBoxType::okCancel, ptr) == DialogBox::messageBoxResponse::ok; });
-
+		Button* button = new Button(L"Create", L"Create dialog", utility::ComponentSettings(0, 0, 200, 20), ptr);
+		
 		ptr->setExitMode(BaseComposite::exitMode::quit);
 		
 		ptr->setLargeIcon(R"(assets\icon.ico)");
+
+		button->setOnClick([]()
+			{
+				DialogBox::DialogBoxBuilder builder(L"Rofl", L"Name", 400, 400, "dialog");
+
+				builder.addComponent<Button>(L"Button", 200, 20, DialogBox::DialogBoxBuilder::alignment::center);
+
+				DialogBox* dialog = builder.build();
+
+				dynamic_cast<Button*>(dialog->findChild(L"Button"))->setOnClick([dialog]()
+					{
+						dialog->close(10);
+					});
+
+				cout << dialog->showDialog() << endl;
+
+				cout << "Rofel" << endl;
+			});
 
 		holder.runMainLoop();
 	}
